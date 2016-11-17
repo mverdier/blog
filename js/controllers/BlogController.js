bf4a6ee03272f201680f8ed35bc50732c25e02ae
mverdier <mart.verdier@gmail.com>
2016-11-17 16:16:21 +0100

    Overrid backspace key

--- js/controllers/BlogController.js
+++ js/controllers/BlogController.js
@@ -51,5 +51,37 @@ app.controller('BlogController', ['$scope', '$http', '$location', function($scop
 		document.getElementById('list-page').style.display = "block";
 		document.getElementById('article-page').style.display = "none";
 		document.getElementById('breadcrumbs').style.display = "none";
-	}
+	};
+
+	//Overriding the backspace key to use the back() method instead
+	//The joys of a single-page application
+	$(document).unbind('keydown').bind('keydown', function (event) {
+		if (event.keyCode === 8) {
+			var doPrevent = true;
+			var types = ["text", "password", "file", "search", "email", "number", "date", "color", "datetime", "datetime-local", "month", "range", "search", "tel", "time", "url", "week"];
+			var d = $(event.srcElement || event.target);
+			var disabled = d.prop("readonly") || d.prop("disabled");
+			if (!disabled) {
+				if (d[0].isContentEditable) {
+					doPrevent = false;
+				} else if (d.is("input")) {
+					var type = d.attr("type");
+					if (type) {
+						type = type.toLowerCase();
+					}
+					if (types.indexOf(type) > -1) {
+						doPrevent = false;
+					}
+				} else if (d.is("textarea")) {
+					doPrevent = false;
+				}
+			}
+
+			//We prevent it from being used ONLY if an article is open
+			if (doPrevent && document.getElementById('article-page').style.display == "block") {
+				event.preventDefault();
+				back();
+			}
+		}
+	});
 }]);
